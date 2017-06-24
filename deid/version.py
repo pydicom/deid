02fc8181d5a4d0215b97e254ba7b4a1141c55e98
vsoch <vsochat@stanford.edu>
2017-06-23 22:12:09 -0400

    updating version

--- deid/version.py
+++ deid/version.py
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 AUTHOR = 'Vanessa Sochat'
 AUTHOR_EMAIL = 'vsochat@stanford.edu'
 NAME = 'deid'
