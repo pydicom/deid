db73a991db70a28cdc6dfbeb4010638bfbe53aab
vsoch <vsochat@stanford.edu>
2017-08-08 00:19:15 -0400

    print broke python 2.7!

--- deid/dicom/header.py
+++ deid/dicom/header.py
@@ -181,8 +181,7 @@ def replace_identifiers(dicom_files,
         dicom_file = dicom_files[d]
         dicom = read_file(dicom_file,force=force)
         idx = os.path.basename(dicom_file)
-        status = "[%s of %s]:%s" %(d,len(dicom_files),idx)
-        print(status, end='', flush=True)
+        print("[%s of %s]:%s" %(d,len(dicom_files),idx))
         fields = dicom.dir()
 
         # Remove sequences first, maintained in DataStore
