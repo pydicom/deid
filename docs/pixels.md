2b6e4ad1fe61781b745fc9eb7c1ebc0c54550b19
vsoch <vsochat@stanford.edu>
2017-12-23 16:37:02 -0800

    adding dicom deid config / recipe class

--- docs/pixels.md
+++ docs/pixels.md
@@ -1,9 +1,9 @@
 # Cleaning Pixels
 
-At this point, you've possibly obtained identifiers via a [get](get.md) action, and you want to figure out which of your images have pixels burned into the data. For that, we have a [set of pixel functions](../deid/dicom/pixels.py) that mirror the functionality of MIRCTP, specifically the functions to [filter DICOM](http://mircwiki.rsna.org/index.php?title=The_CTP_DICOM_Filter) and then [Anonymize](http://mircwiki.rsna.org/index.php?title=The_CTP_DICOM_Pixel_Anonymizer). The  [DicomPixelAnonymizer.script](https://github.com/johnperry/CTP/blob/master/source/files/scripts/DicomPixelAnonymizer.script) is a rule based list of known machine and modality types, and specific locations in the pixels where annotations are commonly found. The [BurnedInPixels.script](https://github.com/johnperry/CTP/blob/master/source/files/scripts/BurnedInPixelsFilter.script) is a set of filters that, given that an image passes through them, it continues processing. If it fails, then we flag it.
+At this point, you've possibly obtained identifiers via a [get](get.md) action, and you want to figure out which of your images have pixels burned into the data. If you don't want the detalis, jump into our [example script](https://github.com/pydicom/deid/blob/master/examples/dicom/pixels/run-cleaner-client.py). Here we will walk through how this cleaner was derived, and how it works.
 
-### Flagging Burned Images
-If we look at the [BurnedInPixels.script](https://github.com/johnperry/CTP/blob/master/source/files/scripts/BurnedInPixelsFilter.script), we see the following:
+## Inspiration from CTP
+Flagging images with potentially having burned in PHI is based on a well established rule-based approach. We know a concrete list of header fields and known locations with PHI associated with fields in the header, and we can check these fields in any files and then perform cleaning if there is a match. This approach is based on the MIRCTP functions to [filter DICOM](http://mircwiki.rsna.org/index.php?title=The_CTP_DICOM_Filter) and then [Anonymize](http://mircwiki.rsna.org/index.php?title=The_CTP_DICOM_Pixel_Anonymizer). The  [DicomPixelAnonymizer.script](https://github.com/johnperry/CTP/blob/master/source/files/scripts/DicomPixelAnonymizer.script) is a rule based list of known machine and modality types, and specific locations in the pixels where annotations are commonly found. The [BurnedInPixels.script](https://github.com/johnperry/CTP/blob/master/source/files/scripts/BurnedInPixelsFilter.script) is a set of filters that, given that an image passes through them, it continues processing. If it fails, then we flag it. If we look at the script above, we see the following:
 
 ```
     # We continue processing given that:
@@ -47,29 +47,71 @@ The pixels with bounding boxes (0,0,100,20) and (480,200,32,250) should be remov
 
 I'm not entirely sure why these two are separate (as both seem to indicate a flag for an image having PHI) but likely it's because the first group (`BurnedInPixels.script`) indicates header fields that are likely to indicate annotation, but don't carry any obvious mapping to a location. We can think of both as a set of filters, some with a clear location, and others not. TLDR: the second file (`DicomPixelAnonymizer.script`) has both header fields and locations.
 
+## Deid Implementation
+We have a [set of pixel functions](../deid/dicom/pixels) that mirror the functionality of MIRCTP, and we take a similar approach of deriving the rules for this process from a [deid recipe](recipe.md). Our implementeation of a [DicomCleaner](https://github.com/pydicom/deid/blob/master/deid/dicom/pixels/clean.py#L35) generally works as follows:
 
-### Deid Representation
-For our purposes, since we just want to flag images with PHI, we don't need to distinguish fields as the MIRCTP does. Also, given that these criteria are universal (it would be unlikely that we would want to give users control to re-define what is considered PHI) we represent these criteria in the [config.json](../deid/dicom/config.json) provided in the dicom module. The filters look like this:
+1. The user initializes a [Recipe](recipe.md) to configure detecting images with PHI (and possibly cleaning). The recipe has two parts - a set of filters to run over the headers to estimate if an image has burned in pixels (a section that starts with `%filter`), and a list of header cleaning rules (`%header`).
+2. The recipe is used to categorize the images into groups based on the defined lists, or to clean the data.
+3. The user selects some subset of images to continue forward with replacement of identifiers.
+
+To jump right in to using the Dicom Cleaner, see our [example script](https://github.com/pydicom/deid/blob/master/examples/dicom/pixels/run-cleaner-client.py). We will walk through the basics here.
+
+We start by importing the class
 
 ```
-   "filter":{
-
-          "contains":[
- 
-              {"action":"FLAG","field":"ImageType","value": "SAVE"},
-              {"action":"FLAG","field":"DateOfSecondaryCapture", "value":""},
-              {"action":"FLAG","field":"SeriesDescription", "value":"SAVE"},
-              {"action":"FLAG","field":"SecondaryCaptureDeviceManufacturer", "value":""},
-              {"action":"FLAG","field":"SecondaryCaptureDeviceManufacturerModelName", "value":""},
-              {"action":"FLAG","field":"SecondaryCaptureDeviceSoftwareVersions", "value":""},
-              {"action":"FLAG","field":"BurnedInAnnotation", "value":"YES"}
-
-
-      ]
-   }
+from deid.dicom import DicomCleaner, get_files 
+from deid.data import get_dataset
 ```
-     
-The user can call a function to go through a list of images, and determine which are likely to have PHI.
 
+and grabbing a dicom file to work with
 
+```
+dicom_file = get_files(get_dataset('dicom-cookies'))[0]
+```
 
+### Client
+
+We need to instantiate a client once, and can use this client to clean one or more files.
+Note that there are various options for setting default output folders. if you don't set
+an output folder, a temporary directory is created and used.
+
+```
+client = DicomCleaner()
+client = DicomCleaner(output_folder='/home/vanessa/Desktop')
+```
+
+The basic steps we will take are the following:
+
+ - `client.detect(dicom_file)`: detect if the image potentially has burned in pixels
+ - `client.clean()`: clean the areas by writing black pixels, given that coordinates are provided in the recipe.
+ - `client.save_<format>`: save the images to a new dicom or png
+
+### Detect
+
+Detect means using the deid recipe to parse headers. You must run detect before you try to clean. If you don't:
+
+```
+client.clean()
+WARNING Use <deid.dicom.pixels.clean.DicomCleaner object at 0x7fafb70b9cf8>.detect() to find coordinates first.
+```
+
+Once you've run detect, you will get a result that includes any flags triggered:
+
+```
+client.detect(dicom_file)
+
+{'flagged': True,
+'results': [{'coordinates': [],
+  'group': 'blacklist',
+  'reason': ' ImageType missing  or ImageType empty '}]}
+```
+
+### Clean and Save
+After detection, the flags that were triggered are saved with the client, until you override with another file.
+You can now run clean, and save the images to a format that you like. Remember that even with flags, if there are no coordinates associated with the flag, no changes are done to the image.
+
+```
+client.clean()
+client.save_png()
+client.save_dicom()
+```
--- examples/dicom/deid-dicom-example.py
+++ examples/dicom/deid-dicom-example.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
+from deid.dicom import get_files, replace_identifiers
+from deid.utils import get_installdir
+from deid.data import get_dataset
+import os
+
 # This is a complete example of doing de-identifiction. For details, see our docs
 # https://pydicom.github.io/deid
 
 
 # This will get a set of example cookie dicoms
-from deid.dicom import get_files
-from deid.data import get_dataset
-from deid.dicom import replace_identifiers
 base = get_dataset('dicom-cookies')
 dicom_files = get_files(base)
 
@@ -22,31 +24,172 @@ ids = get_identifiers(dicom_files)
 # A cookie tumor example is below
 #**
 
-# Load your custom deid parameters (see deid folder under examples)
-from deid.utils import get_installdir
-from deid.config import load_deid
-import os
+################################################################################
+# The Deid Recipe
+#
+# The process of flagging images comes down to writing a set of filters to
+# check if each image meets some criteria of interest. For example, I might
+# create a filter called "xray" that is triggered when the Modality is CT or XR.
+# We specify these fliters in a simple text file called a "deid recipe." When
+# you work with the functions, you have the choice to instantiate the object
+# in advance, or just provide a path to a recipe file. We will walk through
+# examples  for both below, starting with working with a DeidRecipe object.
+# If you aren't interested in this use case or just want to use a provided
+# deid recipe file, continue to the step to replace_identifiers
+#
+##################################
 
+# Create a DeidRecipe
+from deid.config import DeidRecipe
+recipe = DeidRecipe()
+
+# Since we didn't load a custom deid recipe text file, we get a default
+# WARNING No specification, loading default base deid.dicom
+# You can add custom deid files with .load().
+
+# We can look at the criteria loaded:
+recipe.deid
+
+# You can also provide your own deid recipe file, and in doing so, you
+# won't load a default
 path = os.path.abspath("%s/../examples/deid/" %get_installdir())
+recipe = DeidRecipe(deid=path)
 
-# If you are intereseted to see it, but you don't have to do this,
-# this happens internally
-deid = load_deid(path)
+# You can also choose to load the default base with your own recipe
+recipe = DeidRecipe(deid=path, base=True)
+
+# Or specify a different base entirely. The base is the deid.<tag> in the
+# deid.data folder. So for example, under deid/data/deid.dicom.chest.xray we would
+# do:
+recipe = DeidRecipe(deid=path, base=True, default_base='dicom.xray.chest')
+
+# We can also specify one of the deid recipes provided by the library as our 
+# only to use.
+recipe = DeidRecipe(deid='dicom.xray.chest')
+
+# This is to encourage sharing! If you have a general recipe that others might
+# use, please contribute it to the library.
+
+# You can also load multiple deid files, just put them in a list in the order
+# that you want them loaded.
+
+# To see an entire (raw in a dictionary) recipe just look at
+recipe.deid
+
+# Now let's look at parts of this recipe. There are two categories of things: 
+# - filters: are used for parsing over headers and flagging images 
+# - header: is a set of rules that govern a replacement procedure
+
+################################################################################
+# Format
+################################################################################
+
+recipe.get_format()
+# dicom
 
 
-# Let's add the fields that we specify to add in our deid, a source_id for SOPInstanceUID,
-# and an id for PatientID
-count=0
-updated_ids = dict()
-for image,fields in ids.items():    
+################################################################################
+# Filters
+################################################################################
+
+# To get a complete dict of all filters, key (index) is by filter group
+recipe.get_filters()
+
+# To get the group names
+recipe.ls_filters()
+# ['whitelist', 'blacklist']
+
+# To get a list of specific filters under a group
+recipe.get_filters('blacklist')
+
+
+################################################################################
+# Header Actions
+# A header action is a step (e.g., replace, remove, blank) to be applied to
+# a dicom image header. The headers are also part of the deid recipe, and you
+# don't need to necessarily use header actions and filters at the same time.
+################################################################################
+
+# For the example we were doing above, the recipe didn't have a header section.
+# Let's load one that does.
+recipe = DeidRecipe()
+
+# We can get a complete list of actions
+recipe.get_actions()
+
+# We can filter to an action type
+recipe.get_actions(action='ADD')
+
+#[{'action': 'ADD',
+#  'field': 'IssuerOfPatientID',
+#  'value': 'STARR. In an effort to remove PHI all dates are offset from their original values.'},
+# {'action': 'ADD',
+#  'field': 'PatientBirthDate',
+#  'value': 'var:entity_timestamp'},
+# {'action': 'ADD', 'field': 'StudyDate', 'value': 'var:item_timestamp'},
+# {'action': 'ADD', 'field': 'PatientID', 'value': 'var:entity_id'},
+# {'action': 'ADD', 'field': 'AccessionNumber', 'value': 'var:item_id'},
+# {'action': 'ADD', 'field': 'PatientIdentityRemoved', 'value': 'Yes'}]
+
+# or we can filter to a field
+recipe.get_actions(field='PatientID')
+
+#[{'action': 'REMOVE', 'field': 'PatientID'},
+# {'action': 'ADD', 'field': 'PatientID', 'value': 'var:entity_id'}]
+
+# and logically, both
+recipe.get_actions(field='PatientID', action="REMOVE")
+#  [{'action': 'REMOVE', 'field': 'PatientID'}]
+
+
+################################################################################
+# Replacing Identifiers
+#
+# The %header section of a deid recipe defines a set of actions and associated
+# fields to perform them on. As we saw in the examples above, we could easily
+# view and filter the actions based on the header field or action type.
+#
+# For this next section, we will pretend that we've just extracted ids from
+# our data files (in a dictionary called ids) and we will prepare a second
+# dictionary of updated fields.
+
+##################################
+
+# Load the dummy / example deid
+path = os.path.abspath("%s/../examples/deid/" %get_installdir())
+recipe = DeidRecipe(deid=path)
+
+# What actions are defined?
+recipe.get_actions()
+
+# [{'action': 'ADD', 'field': 'PatientIdentityRemoved', 'value': 'Yes'},
+#  {'action': 'REPLACE', 'field': 'PatientID', 'value': 'var:id'},
+#  {'action': 'REPLACE', 'field': 'SOPInstanceUID', 'value': 'var:source_id'}]
+
+# The above says that we are going to:
+#  ADD a field PatientIdentityRemoved with value Yes
+#  REPLACE PatientID with whatever value is under "id" in our updated lookup
+#  REPLACE SOPInstanceUID with whatever value is under "source_id"
+
+# We have 7 dicom cookie images we loaded above, so we have two options. We can
+# either loop through the dictionary of ids and update values (in this case,
+# adding values to be used as new variables) or we can make a new datastructure
+
+# Let's be lazy and just update the extracted ones
+updated_ids = dict(); count=0
+for image, fields in ids.items():    
     fields['id'] = 'cookiemonster'
     fields['source_id'] = "cookiemonster-image-%s" %(count)
     updated_ids[image] = fields
     count+=1
 
-# Run again, provided the path to deid, and the updated ids
+# You can look at each of the updated_ids entries and see the added variables
+#  'id': 'cookiemonster',
+#  'source_id': 'cookiemonster-image-2'}}
+
+# And then use the deid recipe and updated to create new files
 cleaned_files = replace_identifiers(dicom_files=dicom_files,
-                                    deid=path,
+                                    deid=recipe,
                                     ids=updated_ids)
 
 
