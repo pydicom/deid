683a9e9ccb75062d48e16a1ecb773f94d2ee9392
wetzelj <wetzelj@ccf.org>
2020-03-19 14:14:01 -0400

    Update test_utils.py
    Updated number of expected test files.

--- deid/tests/test_utils.py
+++ deid/tests/test_utils.py
@@ -114,7 +114,7 @@ class TestUtils(unittest.TestCase):
         from deid.utils import recursive_find
 
         found = 0
-        expected = 8
+        expected = 9
         for file in recursive_find(self.pwd, pattern="*.dcm"):
             found += 1
         print("Found %s files" % (found))
@@ -126,7 +126,7 @@ class TestUtils(unittest.TestCase):
         print("Testing recursive find as lit.")
         from deid.utils import recursive_find
 
-        expected = 8
+        expected = 9
         files = list(recursive_find(self.pwd, pattern="*.dcm"))
         found = len(files)
         print("Found %s files" % (len(files)))
