61c8c03754566bbcef1dac1d49bca3f4b9592fb1
Judd Storrs <jstorrs@gmail.com>
2022-09-15 10:15:33 -0400

    Bump pydicom to latest release (2.3.0)

--- deid/version.py
+++ deid/version.py
@@ -14,6 +14,6 @@ LICENSE = "LICENSE"
 INSTALL_REQUIRES = (
     ("matplotlib", {"min_version": None}),
     ("numpy", {"min_version": None}),
-    ("pydicom", {"exact_version": "2.1.1"}),
+    ("pydicom", {"exact_version": "2.3.0"}),
     ("python-dateutil", {"min_version": None}),
 )
