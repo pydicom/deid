5b458d1e46573c0884d4510d564bf5a635ed220b
wetzelj <wetzelj@ccf.org>
2022-11-21 14:41:51 -0500

    Add/remove keep behavior (Previous PR #199 - 0.2.29 rc) (#237)
    * #193 'bump pydicom' - bumped pydicom to 2.2.2, local tests green, changelog update, added PyCharm to gitignore
    * WIP 197 remove + keep combo should keep (#198)
    * #197 'remove and keep combo' - added tests that describe situation where remove overrules keep, and also explicitly show that except is using value as substring/regex match
    * #197 'remove and keep combo' - added quite straightforward "add fields marked with KEEP to skiplist", also fixed docstring on parser#perform_action and removed unused parameter, fixed typo in random place
    * #197 'remove and keep combo' - quotes changed as per code format convention
    * #197 'remove and keep combo' - changed keep list to be separate property, updated docs on action order, added two tests for ensuring blank action order
    * #197 'remove and keep combo' - changed keep fields collector to be more pythonic
    * #197 'remove and keep combo' - more safeguards in keep fields collector, clarified docs upon field that should not be None
    * #197 'remove and keep combo' - changelog update, typo fix, version bump
    * #197 'remove and replace/jitter combo' - added once-evaluated list of fields being jittered or replaced to prevent them from being removed, tests, extended description of @keep property, changelog update, typo fix, version bump
    * #197 'remove and replace/jitter combo' - run black
    * Update deid/version.py - agreed to bump to 0.3
    * Bug with return of derive_ctp_coordinate
    Downstream code expects coordinate definitions to be a comma separated list of values, not a list of ints.
    * Update deid/dicom/parser.py
    Committing suggestion.
    * change derive_ctp_coordinates to private function
    
    Co-authored-by: Gleb <gleb.stsenov@gmail.com>
    Co-authored-by: Vanessasaurus <814322+vsoch@users.noreply.github.com>

--- deid/config/utils.py
+++ deid/config/utils.py
@@ -258,7 +258,7 @@ def parse_filter_group(spec):
     return members
 
 
-def derive_ctp_coordinate(raw):
+def _derive_ctp_coordinate(raw):
     """
     Derive a ctp coordinate from a raw (comma separated) string.
 
@@ -284,9 +284,11 @@ def derive_ctp_coordinate(raw):
     if len(new_coordinate) != 4:
         bot.exit("Coordinates are expected to have length of 4, found %s" % raw)
     xmin, ymin, width, height = new_coordinate
+    new_coordinate[2] = xmin + width
+    new_coordinate[3] = ymin + height
 
     # Translate CTP coordinate to the convention we use
-    return [xmin, ymin, xmin + width, ymin + height]
+    return ",".join([str(i) for i in new_coordinate])
 
 
 def parse_label(section, config, section_name, members, label=None):
@@ -311,14 +313,14 @@ def parse_label(section, config, section_name, members, label=None):
         member = members.pop(0).strip()
 
         if member.lower().startswith("ctpcoordinates"):
-            coordinate = derive_ctp_coordinate(
+            coordinate = _derive_ctp_coordinate(
                 member.replace("ctpcoordinates", "").strip()
             )
             criteria["coordinates"].append([0, coordinate])
             continue
 
         elif member.lower().startswith("ctpkeepcoordinates"):
-            coordinate = derive_ctp_coordinate(
+            coordinate = _derive_ctp_coordinate(
                 member.replace("ctpkeepcoordinates", "").strip()
             )
             criteria["coordinates"].append([1, coordinate])
