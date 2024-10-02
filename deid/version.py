d35105fefb2ef063f52326a253e54369bf9735ef
Sam Maxwell <sam@groundtruthlabs.com>
2024-10-02 13:27:05 +0100

    chore: bump version and update changelog

--- deid/version.py
+++ deid/version.py
@@ -2,7 +2,7 @@ __author__ = "Vanessa Sochat"
 __copyright__ = "Copyright 2016-2023, Vanessa Sochat"
 __license__ = "MIT"
 
-__version__ = "0.3.24"
+__version__ = "0.3.25"
 AUTHOR = "Vanessa Sochat"
 AUTHOR_EMAIL = "vsoch@users.noreply.github.com"
 NAME = "deid"
